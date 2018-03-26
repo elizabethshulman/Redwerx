6cc89dfa916230b127a26cb9ce47cf1b67fa0505
elizabethshulman <ers58@duke.edu>
2018-03-25 22:55:21 -0400

    Can create and delete users; initial links added for navigation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,33 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
   end
+
+  def create
+    @user = User.new(user_params)
+
+    @user.save
+    redirect_to @user
+  end
+
+  def update
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to users_path
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
-  attr_accessor :email, :password
+  validates :email, presence: true
+  validates :password, presence:true
 end
